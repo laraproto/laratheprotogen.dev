2762880d9a7cb5a6e9d1a72bf515b121240e2746
laraproto <laraproto@proton.me>
2025-12-01 12:13:57 +0200

    make, something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Head from '$lib/components/front/Head.svelte';
+</script>
+
+<Head title="Dumb little homepage" />
+
+<h1 class="text-2xl">Temporary homepage</h1>
+<p class="md:w-120 sm:w-96">
+	I wanted to get something put on the homepage for now as this domain's been dead since i stopped
+	using laravel, hey at least it's got some dark mode
+</p>
