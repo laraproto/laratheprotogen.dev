9db49eb7bba3cb0dcfe830d5b2f181e290eee95a
laraproto <laraproto@proton.me>
2025-12-01 12:26:07 +0200

    autism

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,12 @@
 
 <Head title="Dumb little homepage" />
 
-<h1 class="text-2xl">Temporary homepage</h1>
+<h1 class="text-2xl font-bold">Temporary homepage</h1>
 <p class="md:w-120 sm:w-96">
 	I wanted to get something put on the homepage for now as this domain's been dead since i stopped
-	using laravel, hey at least it's got some dark mode
+	using laravel, hey at least it's got some dark mode. I might develop like a commissions portal to
+	replace my <a href="https://buymeacoffee.com/laratheprotogen">buymeacoffee you can find here</a>
+	but I don't know, right now I just want a blog using mdsvex and comments using github (either via
+	giscus or github oauth), I was also considering using
+	<a href="https://fresh.deno.dev">the Deno Fresh full-stack framework</a> but I like svelte
 </p>
