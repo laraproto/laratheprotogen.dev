26ae61b6e7a21cbdc32da78b79990096b82acb15
laraproto <laraproto@proton.me>
2025-12-01 13:11:55 +0200

    support unix socket

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,18 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_URL && (!env.DATABASE_HOST || !env.DATABASE_NAME))
+	throw new Error('Database config missing');
 
-const client = postgres(env.DATABASE_URL);
+let client: ReturnType<typeof postgres> | null = null;
 
-export const db = drizzle(client, { schema });
+if (env.DATABASE_URL) {
+	client = postgres(env.DATABASE_URL);
+} else {
+	client = postgres({
+		host: env.DATABASE_HOST,
+		database: env.DATABASE_NAME
+	});
+}
+
+export const db = drizzle(client!, { schema });
