ae3e653ef71f2c9899091e0a223d08392ff8ce2b
laraproto <laraproto@proton.me>
2025-12-01 13:14:10 +0200

    make drizzle config support it too

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL && (!process.env.DATABASE_HOST || !process.env.DATABASE_NAME))
+	throw new Error('Database config missing');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: process.env.DATABASE_URL
+		? { url: process.env.DATABASE_URL }
+		: {
+				host: process.env.DATABASE_HOST!,
+				database: process.env.DATABASE_NAME!
+			},
 	verbose: true,
 	strict: true
 });
