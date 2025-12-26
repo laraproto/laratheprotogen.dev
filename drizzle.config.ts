57a34c210bc5cbd11712dad2255724556b4719de
laraproto <laraproto@proton.me>
2025-12-26 20:51:55 +0200

    fuck ssl tbh

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
-import * as fs from 'fs';
-import * as path from 'path';
 
 if (!process.env.DATABASE_URL && (!process.env.DATABASE_HOST || !process.env.DATABASE_NAME))
 	throw new Error('Database config missing');
-
-const certificate = process.env.DATABASE_CA
-	? fs.readFileSync(process.env.DATABASE_CA).toString()
-	: undefined;
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
 	dbCredentials: process.env.DATABASE_URL
 		? {
-				url: process.env.DATABASE_URL,
-				ssl: {
-					rejectUnauthorized: true,
-					ca: certificate
-				}
+				url: process.env.DATABASE_URL
 			}
 		: {
 				host: process.env.DATABASE_HOST!,
